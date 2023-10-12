614817cfe4b80ed23222ed2e0f7938545106337a
haodn98 <134230717+haodn98@users.noreply.github.com>
2023-10-12 23:00:34 +0200

    Commit 2

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String boy = "Mike";
-        System.out.println("Hello " + boy);
+        double x = 3.0 , y = 4.0;
+        System.out.println("Hello " + (x+y));
     }
 }
